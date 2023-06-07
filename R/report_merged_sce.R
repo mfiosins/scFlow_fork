a6b428df19bf348b3a68d182ca01b5ad37f2def1
mfiosins <maksims.fiosins@gmail.com>
2023-06-07 12:18:30 +0200

    knit_root_dir is not relevant

--- R/report_merged_sce.R
+++ R/report_merged_sce.R
@@ -58,7 +58,6 @@ report_merged_sce <- function(sce,
     ),
     output_dir = report_folder_path,
     output_file = report_file,
-    knit_root_dir = krd,
     intermediates_dir = intd,
     quiet = TRUE
   )
